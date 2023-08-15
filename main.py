c7aabffe397cb05c1c645f2a89d36a49d1319b97
XtremeTHN22 <nigthmaresans2@gmail.com>
2023-08-14 18:53:59 -0600

    debug

--- main.py
+++ main.py
@@ -58,10 +58,8 @@ def get_servers():
         servers = {"servers": []}
         logger.info("Removing passwords from the json file...")
         for x in servers["servers"]:
-            print(x)
             del x["password"]
-            print(x, servers["servers"][x])
-            logger.info("{}{}".format(x, servers["servers"][x]))
+            logger.info("s {}{}".format(x, servers["servers"][x]))
             servers["servers"].append(x)
         
         logger.info("Successfuly removed passwords from the json file.")
